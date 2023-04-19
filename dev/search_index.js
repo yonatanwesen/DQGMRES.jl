f9d0c4c17079a38f0b98a28e7727cbca5daf5586
Documenter.jl <documenter@juliadocs.github.io>
2023-04-19 21:54:56 +0000

    build based on 6afe497

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DQGMRES","category":"page"},{"location":"#DQGMRES","page":"Home","title":"DQGMRES","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DQGMRES.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DQGMRES]","category":"page"}]
+}
