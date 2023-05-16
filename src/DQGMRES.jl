ebda9a24ea92c00171e8eb0fda51081203ae6278
yonatanwesen <yonatanwesenyeleh1@gmail.com>
2023-05-16 16:00:34 -0400

    checking if the k within bounds

--- src/DQGMRES.jl
+++ src/DQGMRES.jl
@@ -46,8 +46,26 @@ function Arnoldi(A,b,k)
 
 end
 
+"""
+#Input arguments
 
-function dqgmres(A,b,k; M=I,N = I)
+A : a square matrix of size mxn
+b : a vector of dimension n
+k : the number of previous vectors to orthogonalize against
+M : Right Preconditioner
+N : Left Preconditioner
+
+x, num_iterations = dqgmres(A,b,k;M,N)
+
+x : solution for system Ax = b
+num_iterations : num_iterations
+
+
+"""
+const FloatOrComplex{T} = Union{T, Complex{T}} where T <: AbstractFloat
+
+
+function my_dqgmres(A,b::AbstractVector{T},k; M=I,N = I) where T <: AbstractFloat
       m,n = size(A)
       if n != m
         throw(DimensionMismatch("A has to be a square matrix!"))
@@ -57,24 +75,25 @@ function dqgmres(A,b,k; M=I,N = I)
         throw(DimensionMismatch("the length of b doesn't match the input of A"))
       end
       #setting up workspace
-      P = zeros(Float64,m,k)
-      Q = zeros(Float64,m,k)
-      H = zeros(Float64,k+1)
-      c = zeros(Float64,k)
-      s = zeros(Float64,k)
+      k_c = min(k,m)
+      P = zeros(T,m,k_c)
+      Q = zeros(T,m,k_c)
+      H = zeros(T,k_c+1)
+      c = zeros(T,k_c)
+      s = zeros(T,k_c)
 
       #iteration counter
       count=0
       cmax = 2*n
-      abstol = √eps(Float64)
-      reltol = √eps(Float64)
+      abstol = √eps(T)
+      reltol = √eps(T)
     
       #check if the left and right preconditioning are identity or not
       NisI = (N==I)
       MisI = (M==I)
 
       
-      x = zeros(Float64,n)
+      x = zeros(T,n)
       r0 = MisI ? b - A*x : M*(b-A*x)
       r_norm= norm(r0,2)
       tol = abstol + r_norm*reltol
@@ -88,14 +107,14 @@ function dqgmres(A,b,k; M=I,N = I)
          #update the iteration counter
          count = count + 1
          # pos for v_i in circular stacks
-         j = mod(count-1,k) + 1 #index corresponding to v_j,p_j in circular stacks P and V
-         j_next = mod(count,k) + 1 # index corresponding to v_j+1
+         j = mod(count-1,k_c) + 1 #index corresponding to v_j,p_j in circular stacks P and V
+         j_next = mod(count,k_c) + 1 # index corresponding to v_j+1
 
          #do the incomplete Arnoldi procedure
          w = NisI ? A*Q[:,j] : A*N*Q[:,j]
          w = MisI ? w : M*w
-         for i = max(1,count-k+1) : count
-            ipos = mod(i-1,k) +1 #pos corrosponding to v_i 
+         for i = max(1,count-k_c+1) : count
+            ipos = mod(i-1,k_c) +1 #pos corrosponding to v_i 
             #h_ij pos in H
             ij = count - i +1 
             q_i = view(Q,:,ipos)
@@ -114,35 +133,26 @@ function dqgmres(A,b,k; M=I,N = I)
             end=#
          end
          #v_i'*v_j = 0  for |i-j| < k so we don't want to use r_(j-1-k),(j-1) when we compute r_(j-k,j) for j≥k+1
-         if count ≥ k+2
-            H[k+1] = 0.0
+         if count ≥ k_c+2
+            H[k_c+1] = 0.0
          end
 
          #update the QR factorzation of H_k
-         for i = max(1,count - k): count-1
-            ipos = mod(i-1,k) +1 
+         for i = max(1,count - k_c): count-1
+            ipos = mod(i-1,k_c) +1 
             ij = count - i 
             ij_n  = ij + 1
             htmp = c[ipos]*H[ij_n] + s[ipos]*H[ij]
             H[ij] = s[ipos]*H[ij_n] - c[ipos]*H[ij]
             H[ij_n] = htmp
          end
-
-         #calculate and apply Ω_k givens rotation
-         #println(H)
          (c[j],s[j],H[1]) = LinearAlgebra.givensAlgorithm(H[1],h_jn)
-         #println("after")
-         #println(H)
          γ_n = s[j]*γ_0
          γ_0 = c[j]*γ_0
-
-         #compute p_j
-         #println("before")
-         #println(P[:,j])
          p_j = view(P,:,j)
          #println(p_j)
-         for i = max(1,count - k): count-1
-            ipos = mod(i-1,k) + 1
+         for i = max(1,count - k_c): count-1
+            ipos = mod(i-1,k_c) + 1
             ij = count - i + 1
             if ipos == j
                 p_j .= p_j .* (-H[ij]) 
@@ -154,14 +164,8 @@ function dqgmres(A,b,k; M=I,N = I)
          #println(p_j)
          p_j .= NisI ? p_j + view(Q,:,j) : p_j + N*view(Q,:,j)
          p_j .= p_j ./ H[1] 
-         #=if any(isnan.(p_j))
-            println("got nans in p_j after dividing by H[1]")
-         end=#
          x = x + γ_0*p_j
          r_norm = abs(γ_n)
-
-
-         #update γ_0
          γ_0 = γ_n
         end
     return x,count
